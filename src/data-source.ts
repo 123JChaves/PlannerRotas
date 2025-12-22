5e5da2f139f684fbe32af24005a6f357ab623ae5
123JChaves <j.blues@hotmail.com>
2025-12-22 01:13:35 -0300

    ifo

--- src/data-source.ts
+++ src/data-source.ts
@@ -19,7 +19,7 @@ export const AppDataSource = new DataSource({
     host: "localhost",
     port: 3306,
     username: "root",
-    password: "123456",
+    password: "",
     database: "plannerrotas",
     synchronize: true,
     logging: true,
